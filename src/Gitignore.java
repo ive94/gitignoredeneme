bcbf861954097c638e1da7af5921957f93a8b383
ilkerVefa <ilkervefaedisan@gmail.com>
2022-08-16 22:54:34 +0300

    ASD

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -3,6 +3,7 @@ public class Gitignore {
     public static void main(String[] args) {
 
         System.out.println("Gitignore deneme 1");
+        System.out.println("Gitignore deneme branch");
 
     }
 }
