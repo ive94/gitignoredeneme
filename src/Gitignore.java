ed0fcc950fe931336c82493c1bee73d049cb33cf
ilkerVefa <ilkervefaedisan@gmail.com>
2022-08-16 23:12:08 +0300

    B1

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -5,6 +5,7 @@ public class Gitignore {
         System.out.println("Gitignore deneme 1");
          System.out.println("Gitignore deneme 2");
          System.out.println("Gitignore deneme branch");
+         System.out.println("Gitignore deneme branch 2");
 
 
     }
